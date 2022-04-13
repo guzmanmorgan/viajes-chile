dcbd5c2495089ecc98f168e4514f0259889695e2
manuel <manuelguzmanvilches@gmail.com>
2022-04-13 11:16:36 -0400

    agregando tooltip y modal en HTML y JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,26 @@
 $(document).ready(function () {
 
-
+	//CAMBIO DE COLOR EN BARRA NAVBAR
 	$(window).scroll(function () {
 		$('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
 	});
 
+	// TOOLTIP BOOSTRAP 5.1
+	var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+	var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+		return new bootstrap.Tooltip(tooltipTriggerEl)
+	})
 
-	
+	// MODAL BOOSTRAP 5.1
+	$('#myModal').on('shown.bs.modal', function () {
+		$('#myInput').trigger('focus')
+	})
+
+
+
+
+
+
 
 
 });
